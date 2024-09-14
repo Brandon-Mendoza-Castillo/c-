eb5b38891d44afb791dc5ec4104c5ba37f266870
Brandon-Mendoza-Castillo <160100654+Brandon-Mendoza-Castillo@users.noreply.github.com>
2024-09-14 14:05:56 -0600

    Add files via upload

---
+++ Numero diferente de 0.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    // Declarar dos variables dobles y asignarles valores
+    double numero1 = 3.14;
+    double numero2 = 2.718;
+
+    // Mostrar los valores de las variables
+    std::cout << "El valor de la primera variable es: " << numero1 << std::endl;
+    std::cout << "El valor de la segunda variable es: " << numero2 << std::endl;
+
+    return 0;
+}
+
