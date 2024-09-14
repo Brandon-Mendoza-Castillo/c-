1445ff66ce824f686d00ff352ca7a94c341f5548
Brandon-Mendoza-Castillo <160100654+Brandon-Mendoza-Castillo@users.noreply.github.com>
2024-09-13 23:24:44 -0600

    Add files via upload

---
+++ entero.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    // Declaración de variables
+    int variable1, variable2, variable3;
+
+    // Asignación de valores enteros
+    variable1 = 10;
+    variable2 = 20;
+    variable3 = 30;
+
+    // Imprimir los valores asignados
+    std::cout << "Variable 1: " << variable1 << std::endl;
+    std::cout << "Variable 2: " << variable2 << std::endl;
+    std::cout << "Variable 3: " << variable3 << std::endl;
+
+    return 0;
+}
