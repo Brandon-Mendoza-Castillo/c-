0225d69a2402b0509487b4def64bca0966c971ae
Brandon-Mendoza-Castillo <160100654+Brandon-Mendoza-Castillo@users.noreply.github.com>
2024-09-14 14:06:22 -0600

    Add files via upload

---
+++ Pract#.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main (){
+	int var = 1;
+	int con = 1;
+	// es verdaderamente var =var +1 
+	printf ("%d",++var);
+	printf ("%d", var);
+	// es equivalente con = con-1
+	printf ("%d",--con);
+	printf ("%d",con);
+	
+	
+	system("Pause");
+	return 0;
+}
